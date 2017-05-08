8827fa82a30ff628a5c82e1d6399a19f91675386
Jacky Tjoa <jacky.coolheart@gmail.com>
2017-05-08 21:54:53 +0800

    Update to swift 3

--- MPController/ViewController.swift
+++ MPController/ViewController.swift
@@ -26,21 +26,21 @@ class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessi
     var session:MCSession!
     var browser:MCBrowserViewController!
     
-    var touchDelta = CGPointZero
+    var touchDelta = CGPoint.zero
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         //UIGestureRecognizer
-        let panGesture = UIPanGestureRecognizer(target: self, action: "handlePan:")
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(ViewController.handlePan(_:)))
         joypadHead.addGestureRecognizer(panGesture)
         
         //Multipeer Connectivity
         
         //session
-        self.myPeerID = MCPeerID(displayName: UIDevice.currentDevice().name)
-        self.session = MCSession(peer: self.myPeerID, securityIdentity: nil, encryptionPreference: .Required)
+        self.myPeerID = MCPeerID(displayName: UIDevice.current.name)
+        self.session = MCSession(peer: self.myPeerID, securityIdentity: nil, encryptionPreference: .required)
         self.session.delegate = self
         self.browser = MCBrowserViewController(serviceType: kServiceType, session: self.session)
         self.browser.delegate = self
@@ -51,28 +51,28 @@ class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessi
         // Dispose of any resources that can be recreated.
     }
     
-    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-        return UIInterfaceOrientationMask.Landscape
+    override var supportedInterfaceOrientations : UIInterfaceOrientationMask {
+        return .landscape
     }
     
-    override func shouldAutorotate() -> Bool {
+    override var shouldAutorotate : Bool {
         return true
     }
     
     //MARK:- IBActions
 
-    @IBAction func browseTapped(sender: AnyObject) {
+    @IBAction func browseTapped(_ sender: AnyObject) {
     
-        self.presentViewController(self.browser, animated: true, completion: nil)
+        self.present(self.browser, animated: true, completion: nil)
     }
     
-    @IBAction func actionTapped(sender: AnyObject) {
+    @IBAction func actionTapped(_ sender: AnyObject) {
      
-        let dict = ["type":"button", "index":NSNumber(int: 0)]
-        let dictData = NSKeyedArchiver.archivedDataWithRootObject(dict)
+        let dict = ["type":"button", "index":NSNumber(value: 0 as Int32)] as [String : Any]
+        let dictData = NSKeyedArchiver.archivedData(withRootObject: dict)
         
         do {
-            try self.session.sendData(dictData, toPeers: self.session.connectedPeers, withMode: .Reliable)
+            try self.session.send(dictData, toPeers: self.session.connectedPeers, with: .reliable)
         } catch {
             print("error sending data")
         }
@@ -80,19 +80,19 @@ class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessi
     
     //MARK:- UIGestureRecognizers
     
-    func handlePan(recognizer:UIPanGestureRecognizer) {
+    func handlePan(_ recognizer:UIPanGestureRecognizer) {
     
-        let touchPoint = recognizer.locationInView(self.view)
+        let touchPoint = recognizer.location(in: self.view)
         
         if(recognizer.view == self.joypadHead) {
         
-            if(recognizer.state == .Began) {
+            if(recognizer.state == .began) {
         
-                touchDelta = CGPointMake(
-                    touchPoint.x - self.joypadBase.center.x,
-                    touchPoint.y - self.joypadBase.center.y)
+                touchDelta = CGPoint(
+                    x: touchPoint.x - self.joypadBase.center.x,
+                    y: touchPoint.y - self.joypadBase.center.y)
             }
-            else if(recognizer.state == .Changed) {
+            else if(recognizer.state == .changed) {
             
                 let diffFromBaseX = self.joypadHead.center.x - self.joypadBase.center.x
                 let diffFromBaseY = self.joypadHead.center.y - self.joypadBase.center.y
@@ -112,34 +112,34 @@ class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessi
                 
                 if (radius < 70)
                 {
-                    self.joypadHead.center = CGPointMake(
-                        touchPoint.x - touchDelta.x,
-                        touchPoint.y - touchDelta.y)
+                    self.joypadHead.center = CGPoint(
+                        x: touchPoint.x - touchDelta.x,
+                        y: touchPoint.y - touchDelta.y)
         
                 } else {
                     
                     let maxRadius:Float = 70.5
                     
-                    self.joypadHead.center = CGPointMake(
-                        self.joypadBase.center.x - CGFloat(cos(touchAngle) * maxRadius),
-                        self.joypadBase.center.y - CGFloat(sin(touchAngle) * maxRadius))
+                    self.joypadHead.center = CGPoint(
+                        x: self.joypadBase.center.x - CGFloat(cos(touchAngle) * maxRadius),
+                        y: self.joypadBase.center.y - CGFloat(sin(touchAngle) * maxRadius))
                 }
                 
                 if self.session.connectedPeers.count == 1 {
                 
-                    let dict = ["type":"move", "touchDistance":NSNumber(float: touchDistance), "touchAngle":NSNumber(float: touchAngle)]
-                    let dictData = NSKeyedArchiver.archivedDataWithRootObject(dict)
+                    let dict = ["type":"move", "touchDistance":NSNumber(value: touchDistance as Float), "touchAngle":NSNumber(value: touchAngle as Float)] as [String : Any]
+                    let dictData = NSKeyedArchiver.archivedData(withRootObject: dict)
                     
                     do {
-                        try self.session.sendData(dictData, toPeers: self.session.connectedPeers, withMode: .Reliable)
+                        try self.session.send(dictData, toPeers: self.session.connectedPeers, with: .reliable)
                     } catch {
                         print("error sending data")
                     }
                 }
             }
-            else if(recognizer.state == .Ended) {
+            else if(recognizer.state == .ended) {
                 
-                UIView.animateWithDuration(0.5, animations: { () -> Void in
+                UIView.animate(withDuration: 0.5, animations: { () -> Void in
                     
                     recognizer.view?.center = self.joypadBase.center
                 })
@@ -149,7 +149,7 @@ class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessi
     
     //MARK:- Util
     
-    func calcDistanceWithOrigin(origin:CGPoint, andDestination destination:CGPoint) -> CGFloat {
+    func calcDistanceWithOrigin(_ origin:CGPoint, andDestination destination:CGPoint) -> CGFloat {
     
         let deltaX = destination.x - origin.x
         let deltaY = destination.y - origin.y
@@ -159,59 +159,59 @@ class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessi
     
     //MARK: - MCNearbyServiceBrowserDelegate
     
-    func browserViewController(browserViewController: MCBrowserViewController, shouldPresentNearbyPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) -> Bool {
+    func browserViewController(_ browserViewController: MCBrowserViewController, shouldPresentNearbyPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) -> Bool {
         
         print("peerID: \(peerID)")
         
         return true
     }
     
-    func browserViewControllerDidFinish(browserViewController: MCBrowserViewController) {
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
         
         print("browser finished")
         
-        self.browser.dismissViewControllerAnimated(true, completion: nil)
+        self.browser.dismiss(animated: true, completion: nil)
     }
     
-    func browserViewControllerWasCancelled(browserViewController: MCBrowserViewController) {
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
         
         print("browser cancelled")
         
-        self.browser.dismissViewControllerAnimated(true, completion: nil)
+        self.browser.dismiss(animated: true, completion: nil)
     }
     
     //MARK: - MCSessionDelegate
     
-    func session(session: MCSession, didReceiveCertificate certificate: [AnyObject]?, fromPeer peerID: MCPeerID, certificateHandler: (Bool) -> Void) {
+    func session(_ session: MCSession, didReceiveCertificate certificate: [AnyObject]?, fromPeer peerID: MCPeerID, certificateHandler: (Bool) -> Void) {
         
         return certificateHandler(true)
     }
     
-    func session(session: MCSession, peer peerID: MCPeerID, didChangeState state: MCSessionState) {
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
         
         print("myPeerID: \(self.session.myPeerID)")
         print("connectd peerID: \(peerID)")
         
         switch state {
             
-        case .Connecting:
+        case .connecting:
             print("Connecting..")
             break
             
-        case .Connected:
+        case .connected:
             print("Connected..")
             
-            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            DispatchQueue.main.async(execute: { () -> Void in
                 
                 self.statusLbl.text = self.kTextConnected
             })
             
             break
             
-        case .NotConnected:
+        case .notConnected:
             print("Not Connected..")
             
-            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            DispatchQueue.main.async(execute: { () -> Void in
                 
                 self.statusLbl.text = self.kTextNotConnected
             })
@@ -220,22 +220,22 @@ class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessi
         }
     }
     
-    func session(session: MCSession, didReceiveData data: NSData, fromPeer peerID: MCPeerID) {
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
         
         print("hand didReceiveData")
     }
     
-    func session(session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, withProgress progress: NSProgress) {
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
         
         print("hand didStartReceivingResourceWithName")
     }
     
-    func session(session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, atURL localURL: NSURL, withError error: NSError?) {
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL, withError error: Error?) {
         
         print("hand didFinishReceivingResourceWithName")
     }
     
-    func session(session: MCSession, didReceiveStream stream: NSInputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
         
         print("hand didReceiveStream")
     }
